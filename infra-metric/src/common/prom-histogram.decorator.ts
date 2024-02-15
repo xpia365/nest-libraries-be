001d4c5e17b428077e33114140ef9b6cc22af237
luandevpro <luandevpro@gmail.com>
2024-02-15 11:07:08 +0700

    fix: config commit vs lint format

--- infra-metric/src/common/prom-histogram.decorator.ts
+++ infra-metric/src/common/prom-histogram.decorator.ts
@@ -2,22 +2,25 @@ import { createParamDecorator } from '@nestjs/common';
 import { findOrCreateHistogram } from './prom.utils';
 import { IMetricArguments } from '../interfaces';
 
-export const PromHistogram = createParamDecorator((data: IMetricArguments | string) => {
+export const PromHistogram = createParamDecorator(
+  (data: IMetricArguments | string) => {
+    const arg: Partial<IMetricArguments> = {};
 
-  const arg: Partial<IMetricArguments> = {};
+    if (typeof data === 'string') {
+      arg.name = data;
+    }
+    if (typeof data === 'object') {
+      arg.name = data.name;
+      arg.help = data?.help;
+      arg.labelNames = data.labelNames;
+    }
 
-  if (typeof data === 'string') {
-    arg.name = data;
-  }
-  if (typeof data === 'object') {
-    arg.name = data.name;
-    arg.help = data?.help;
-    arg.labelNames = data.labelNames;
-  }
+    if (!arg.name || arg.name.length === 0) {
+      throw new Error(
+        `PromHistogram need an argument, must be a fulfilled string or IPromHistogramDecoratorArg instance`,
+      );
+    }
 
-  if (!arg.name || arg.name.length === 0) {
-    throw new Error(`PromHistogram need an argument, must be a fulfilled string or IPromHistogramDecoratorArg instance`);
-  }
-
-  return findOrCreateHistogram(arg as IMetricArguments);
-});
+    return findOrCreateHistogram(arg as IMetricArguments);
+  },
+);
--- infra-metric/src/common/prom.decorators.ts
+++ infra-metric/src/common/prom.decorators.ts
@@ -5,25 +5,29 @@ import { IMetricArguments, CounterMetric } from '../interfaces';
  * Create and increment a counter when the method is called
  */
 export const PromMethodCounter = (params?: IMetricArguments) => {
-    return (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<(...args: any[]) => any>) => {
-        const className = target.constructor.name;
-        const name = `app_${className}_${propertyKey.toString()}_calls_total`;
-        const help = `app_${className}#${propertyKey.toString()} called total`;
-        let counterMetric: CounterMetric;
-        const methodFunc = descriptor.value;
-        descriptor.value = function (...args: any[]) {
-            if (!counterMetric) {
-                counterMetric = findOrCreateCounter({ 
-                    name,
-                    help,
-                    ...params,
-                });
-            }
-            counterMetric.inc(1);
-            return methodFunc.apply(this, args);
-        };
+  return (
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: TypedPropertyDescriptor<(...args: any[]) => any>,
+  ) => {
+    const className = target.constructor.name;
+    const name = `app_${className}_${propertyKey.toString()}_calls_total`;
+    const help = `app_${className}#${propertyKey.toString()} called total`;
+    let counterMetric: CounterMetric;
+    const methodFunc = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      if (!counterMetric) {
+        counterMetric = findOrCreateCounter({
+          name,
+          help,
+          ...params,
+        });
+      }
+      counterMetric.inc(1);
+      return methodFunc.apply(this, args);
     };
-}
+  };
+};
 
 /**
  * Create and increment a counter when a new instance is created
@@ -31,24 +35,24 @@ export const PromMethodCounter = (params?: IMetricArguments) => {
  * @param ctor
  */
 export const PromInstanceCounter = (params?: IMetricArguments) => {
-    return <T extends { new(...args: any[]): {} }>(ctor: T) => {
-        const name = `app_${ctor.name}_instances_total`;
-        const help = `app_${ctor.name} object instances total`;
-        let counterMetric: CounterMetric;
+  return <T extends { new (...args: any[]): {} }>(ctor: T) => {
+    const name = `app_${ctor.name}_instances_total`;
+    const help = `app_${ctor.name} object instances total`;
+    let counterMetric: CounterMetric;
 
-        return class extends ctor {
-            constructor(...args) {
-                if (!counterMetric) {
-                    counterMetric = findOrCreateCounter({
-                        name,
-                        help,
-                        ...params,
-                    });
-                }
-
-                counterMetric.inc(1);
-                super(...args);
-            }
+    return class extends ctor {
+      constructor(...args) {
+        if (!counterMetric) {
+          counterMetric = findOrCreateCounter({
+            name,
+            help,
+            ...params,
+          });
         }
+
+        counterMetric.inc(1);
+        super(...args);
+      }
     };
-}
+  };
+};
